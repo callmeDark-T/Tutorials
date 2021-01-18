44e7007be5d661c461f8a277e1a4734f83166723
Tamajong Asamba Aiden <devops@cashrulez-entertainment.com>
2021-01-18 06:10:21 +0100

    Luv2Code Typescrit Training

---
+++ Luv2Code/Typescript-Training/02-variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Tamajong Asamba";
+var lastName = 'Aiden';
+console.log(found);
+console.log("The grade is " + grade);
+console.log(" Hi " + firstName + " " + lastName);
---
+++ Luv2Code/Typescript-Training/02-variables/sample-types.ts
@@ -0,0 +1,8 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Tamajong Asamba";
+let lastName: string = 'Aiden';
+
+console.log(found);
+console.log("The grade is "+grade);
+console.log(` Hi ${firstName} ${lastName}`);
